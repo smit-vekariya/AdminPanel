a9f6087a01cd1a01af90d30b2d2926756082c3e1
smit <smitvekariya164@gmail.com>
2023-10-02 09:03:45 +0530

    add scaping function

--- Backend/AdminPanel/ApiApp/views.py
+++ Backend/AdminPanel/ApiApp/views.py
@@ -34,29 +34,34 @@ def get_movies(request):
 
 @csrf_exempt
 def movie_details(request):
-     movie_id = request.POST.get("movie_id")
-     movies = MovieInfo.objects.filter(id=movie_id).values("id","name","slug","release_date","trailer_url","download_url","thumbnail_url","source_url","screenshots","source_type__name","duration","description","language","genres","cast","published")
-     movies_info = {"data":[]}
-     for data in movies:
-          movies_info["data"].append({
-               "id":data["id"],
-               "name":data["name"],
-               "slug":data["slug"],
-               "release_date":str(data["release_date"]),
-               "trailer_url":data["trailer_url"],
-               "download_url":data["download_url"],
-               "thumbnail_url":data["thumbnail_url"],
-               "source_url":data["source_url"],
-               "screenshots":data["screenshots"],
-               "source_type":data["source_type__name"],
-               "duration":data["duration"],
-               "description":data["description"],
-               "language":str(data["language"]),
-               "genres":data["genres"],
-               "cast":data["cast"],
-          })
+     try:
+          movie_id = json.loads(request.body)["movie_id"]
+          movies = MovieInfo.objects.filter(id=movie_id).values("id","name","slug","release_date","trailer_url","download_url","thumbnail_url","source_url","screenshots","source_type__name","duration","description","language","genres","cast","published")
+          movies_info = {"data":{}, "status": 1, "message": "success"}
+          for data in movies:
+               movies_info["data"].update({
+                    "id":data["id"],
+                    "name":data["name"],
+                    "slug":data["slug"],
+                    "release_date":str(data["release_date"]),
+                    "trailer_url":data["trailer_url"],
+                    "download_url":data["download_url"],
+                    "thumbnail_url":data["thumbnail_url"],
+                    "source_url":data["source_url"],
+                    "screenshots":data["screenshots"],
+                    "source_type":data["source_type__name"],
+                    "duration":data["duration"],
+                    "description":data["description"],
+                    "language":str(data["language"]),
+                    "genres":data["genres"],
+                    "cast":data["cast"],
+               })
+          return HttpResponse(json.dumps(movies_info))
+     except Exception as e:
+          return HttpResponse(json.dumps({"data":{}, "status": 0, "message": str(e)}))
 
-     return HttpResponse(json.dumps(movies_info))
+
+
 
 
 @csrf_exempt
@@ -157,3 +162,56 @@ def movie_search(request):
           return HttpResponse(json.dumps(movies_info))
      else:
           return HttpResponse(json.dumps("Movie not found! Please try again later."))
+
+
+
+
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+
+import codecs
+
+import re
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+val = "https://cyberfile.me/b7ip"
+
+wait = WebDriverWait(driver, 10)
+
+driver.get(val)
+
+
+get_url = driver.current_url
+# wait.until(EC.url_to_be(val))
+wait.until_not(EC.url_to_be(val))
+
+print(get_url)
+print(val)
+if get_url == val:
+    print("-----------")
+
+
+page_source = driver.page_source
+# print(page_source)
+soup = BeautifulSoup(page_source,features="html.parser")
+pp =soup.find('div', class_='responsiveMobileMargin')
+ps= str(pp)
+ps2 = ps.split("openUrl('")[1]
+ps3 = ps2.split("');")[0]
+print(ps3)
+
+# with open("data/cyberfile_____1__.html", "w") as f:
+#     f.write(page_source)
+
